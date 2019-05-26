81a29134895f7d1a77fbcdb89d48f3dab446a330
Riccardo Della Vecchia <riccardodellavecchia@MBP-di-Riccardo.station>
2019-05-26 12:25:40 +0200

    some numerical stability issues solved

--- LowerBound4d.jl
+++ LowerBound4d.jl
@@ -49,7 +49,7 @@ function f2(k, x, q)
         q[2] q[4] 1-2*x 1]
     # σ = my_id(σ)
     # Σ = σ
-    Σ = Symmetric(σ) + 1e-10I
+    Σ = Symmetric(σ) + 1e-15I
     Σd = det(Σ)
     @assert all(eigvals(Σ) .> 0)
     Σi = inv(Σ)
@@ -82,7 +82,7 @@ function f1(k, x)
 end
 
 function H8(x, η, q...)
-    δ = 1e-10
+    δ = 1e-7 # in the optimization in η it happens that ipopt goes below and above boundaries? seems so
     a = [1/4*(q[1]-q[2]+2*x)-η, 1/4*(-q[3]+q[4]+2*x)-η, 1/4*(2-q[1]-q[4]-4*x)+η,
             1/4*(q[1]-q[3]+2*x)-η, η]
     a0 = 1-a[1]-a[2]-a[3]-x
@@ -92,8 +92,10 @@ function H8(x, η, q...)
     a0 = (-δ < a0 < 0) ?  0. : a0
     a6 = (-δ < a6 < 0) ?  0. : a6
     a7 = (-δ < a7 < 0) ?  0. : a7
-    # println("a = $a\ta0 = $a0\ta6 = $a6\ta7 = $a7")
-    for i in 1:5 @assert 0<=a[i]<=1. end
+    #println("a = $a\ta0 = $a0\ta6 = $a6\ta7 = $a7\tη = $η")
+    #for i in 1:5 @assert 0<=a[i]<=1. end
+    @assert 0<=a[1]<=1.; @assert 0<=a[2]<=1.; @assert 0<=a[3]<=1.;
+    @assert 0<=a[4]<=1.; @assert 0<=a[5]<=1.;
     @assert 0<=a0<=1.
     @assert 0<=a6<=1.
     @assert 0<=a7<=1.
@@ -104,31 +106,28 @@ end
 
 function compute_supH8(x, q, minB, maxA)
     model = Model(with_optimizer(Ipopt.Optimizer, print_level=0))
-    @NLparameter(model, xx == x)
-    @NLparameter(model, qq[i=1:4] == q[i])
     register(model, :xlogx, 1, xlogx, autodiff=true)
     register(model, :H8, 6, H8, autodiff=true)
-
+    @NLparameter(model, xx == x)
+    @NLparameter(model, qq[i=1:4] == q[i])
     η = @variable(model, base_name="η", lower_bound=maxA, upper_bound=minB)
-    #set_start_value(η, (maxA+minB)/2)
+    set_start_value(η, (maxA+minB)/2)
     @NLobjective(model, Max, H8(xx, η, qq...))
     optimize!(model)
     return  objective_value(model)
 end
 
-function αLB(h2, logf1, supH8, k, x, q)
-    (log(2)+2*h2-supH8)/(log(f2(k, x, q))-2*logf1)
-end
-
 function αLB(k, x, q)
     ok, maxA, minB = is_good(k, x, q)
+    #println("$maxA $minB")
     !ok && return ok, Inf
 
     f1val = f1(k, x)
     f2val = f2(k, x, q)
-    f2val - f1val^2 < 1e-8 && return false, Inf # CHECK This
+    #println("$(f1val^2) $f2val")
+    #f2val - f1val^2 < 1e-8 && return false, Inf # CHECK This
 
-    if abs(minB-maxA) < 1e-5
+    if abs(minB-maxA) < 1e-7
         supH8 = H8(x, (maxA+minB)/2, q...)
     else
         supH8 = compute_supH8(x, q, minB, maxA)
@@ -138,6 +137,7 @@ function αLB(k, x, q)
 end
 
 function is_good(k, x, q)
+    #ϵ = 1e-10
     ok = false
     maxA = max(1/4*(q[1]-q[2]+2*x-4), 1/4*(-4-q[3]+q[4]+2*x), 1/4*(-2+q[1]+q[4]+4*x),
                 1/4*(-4+q[1]-q[3]+2*x), 0., 1/4*(q[1]-q[2]-q[3]+q[4]),
@@ -145,7 +145,7 @@ function is_good(k, x, q)
     minB = min(1/4*(q[1]-q[2]+2*x), 1/4*(-q[3]+q[4]+2*x), 1/4*(2+q[1]+q[4]+4*x),
                 1/4*(q[1]-q[3]+2*x), 1., 1/4*(q[1]-q[2]-q[3]+q[4]+4*x),
                 1/4*(-q[2]+q[4]+2*x), 1/4*(2-q[2]-q[3]))
-
+    #abs(maxA-minB)<ϵ && (minB=maxA)
     ok =  maxA <= minB
     return ok, maxA, minB
 end
@@ -178,7 +178,7 @@ end
 compute_UB(k, x) = -(log(2)+H2(x))/(log(f1(k, x)))
 
 function compute_bounds(n, k)
-    X = vcat([i for i in 1:2:9]*1e-3, [i for i in 1:2:9]*1e-2, [i for i in 1:5]*1e-1)
+    X = vcat([i for i in 1:9]*1e-3, [i for i in 1:9]*1e-2, [i for i in 1:5]*1e-1)
     # X = [i for i in 1:2:9]*1e-3
     bounds = []
     for x in X
@@ -189,21 +189,33 @@ function compute_bounds(n, k)
     return bounds
 end
 
-bounds = compute_bounds(50,1.)
+bounds = compute_bounds(10,1.)
 bounds = hcat(bounds...)
-#display(bounds)
+display(bounds)
 df = DataFrame(x = bounds[1,:], α_LB = bounds[3,:], α_UB = bounds[4,:])
-Gadfly.plot(df, x = :x, y = Col.value(:α_LB,:α_UB), color = Col.index(:α_LB,:α_UB))
-df
+plot = Gadfly.plot(df, x = :x, y = Col.value(:α_LB,:α_UB), color = Col.index(:α_LB,:α_UB))
+plot |> PDF("bounds.pdf")
 
-# some code to sudy the lowe bound RS points
-# αLB(1., 0.001, [0.66666,0.66666,0.66666,0.66666])
+# some code to sudy the lower bound RS points
+# k=1.; x = 0.01
+# af1 = compute_UB(k,x)
+# αLB(k, x, 0.99*ones(4))
+# f2(k, x, 0.9989*ones(4))
+# αLB(k, x, 0.998999*ones(4))
+# is_good(k,x,0.9989*ones(4))
+# H8(x,0.0005,0.9989*ones(4)...)
+# q=0.981*ones(4)
+# 1/4*(2-q[1]-q[4]-4*x)
 #
+# k=1. ; x = 0.01
 # a=[]
-# for q in 0.:0.001:1.
-#     ok, aa = αLB(1., 0.001, q .* ones(4))
-#     global a = push!(a, [q, aa])
-#     println("$q $aa")
+# af1 = compute_UB(k,x)
+# for q in 0.99:0.00001:0.9901
+#     ok, aa = αLB(k, x, q .* ones(4))
+#     global a = push!(a, [q, aa, af1])
+#     println("$q $aa $af1")
 # end
-# a = hcat(a)
-# minimum(a[2,:])
+# b = hcat(a...)
+# minimum(b[2,:])
+# argmin(b[2,:])
+# b[:,17]
